fe15d2fee99633b41700363b9c20d72ffabad2d2
kaleongkong <kaleongkong@yahoo.com>
2014-04-05 16:53:17 -0700

    added Gallery Activity

---
+++ Wordtastic/src/com/example/wordtastic/GalleryActivity.java
@@ -0,0 +1,7 @@
+package com.example.wordtastic;
+
+import android.app.Activity;
+
+public class GalleryActivity extends Activity{
+
+}
--- Wordtastic/src/com/example/wordtastic/Wordtastic_MainActivity.java
+++ Wordtastic/src/com/example/wordtastic/Wordtastic_MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.wordtastic;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
 
 public class Wordtastic_MainActivity extends Activity {
-
+	
+	Button play;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wordtastic__main);
+		play = (Button)findViewById(R.id.play);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.wordtastic__main, menu);
-		return true;
+	protected void onClickPlay(){
+		Intent i = new Intent(this, GalleryActivity.class);
+		startActivity(i);
 	}
 
 }
