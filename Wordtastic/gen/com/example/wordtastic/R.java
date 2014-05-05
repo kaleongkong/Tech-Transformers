35c94ec8fcde6180add0f41701ad38b1a7caff42
Patrick Lin <patrick.lin@berkeley.edu>
2014-05-04 18:25:42 -0700

    changes

--- Wordtastic/gen/com/example/wordtastic/R.java
+++ Wordtastic/gen/com/example/wordtastic/R.java
@@ -29,14 +29,16 @@ public final class R {
         public static final int dolphins=0x7f020005;
         public static final int edited=0x7f020006;
         public static final int editedresultspage=0x7f020007;
-        public static final int greencheckmark=0x7f020008;
-        public static final int home=0x7f020009;
-        public static final int ic_launcher=0x7f02000a;
-        public static final int lion_600x450=0x7f02000b;
-        public static final int redcrossmark=0x7f02000c;
-        public static final int sound_input=0x7f02000d;
-        public static final int table=0x7f02000e;
-        public static final int tree=0x7f02000f;
+        public static final int gearicon=0x7f020008;
+        public static final int greencheckmark=0x7f020009;
+        public static final int home=0x7f02000a;
+        public static final int houseicon=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int lion_600x450=0x7f02000d;
+        public static final int redcrossmark=0x7f02000e;
+        public static final int sound_input=0x7f02000f;
+        public static final int table=0x7f020010;
+        public static final int tree=0x7f020011;
     }
     public static final class id {
         public static final int action_settings=0x7f080034;
